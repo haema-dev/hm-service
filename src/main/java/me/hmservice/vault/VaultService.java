2b596ee7bbcff6b441e2ac4fcd834cf75f2107c5
suajo <sa1.jo@okestro.com>
2024-03-14 13:41:14 +0900

    - Reactive remove - Add ignore

--- src/main/java/me/hmservice/vault/VaultService.java
+++ src/main/java/me/hmservice/vault/VaultService.java
@@ -2,21 +2,23 @@ package me.hmservice.vault;
 
 import lombok.extern.slf4j.Slf4j;
 //import me.hmservice.vault.config.VaultConfig;
+import me.hmservice.domain.client.VaultClient;
 import me.hmservice.vault.config.VaultConfig;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
 
 @Slf4j
 @Service
 public class VaultService {
 
   private final VaultConfig vaultConfig;
+  private final VaultClient vaultClient;
 
-  public VaultService(VaultConfig vaultConfig) {
+  public VaultService(VaultConfig vaultConfig, VaultClient vaultClient) {
     this.vaultConfig = vaultConfig;
+    this.vaultClient = vaultClient;
   }
 
-  public Mono<Object> readSecret() {
-    return vaultConfig.getVault();
+  public Object readSecret() {
+    return vaultClient.getVault(vaultConfig.getToken());
   }
 }
